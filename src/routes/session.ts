10b0bc65f1a39e4c9951b75382fa64d9bfd8c4fc
Raam <raam.sanghani@gmail.com>
2025-12-12 16:24:58 -0600

    create.ts module has officially been deleted. Replaced original /session/preset and /session/custom POST routes with the v2 controllers.

--- src/routes/session.ts
+++ src/routes/session.ts
@@ -1,5 +1,5 @@
 import Router from "express";
-import { createPresetSession, createCustomSession,  } from "./controllers/session/create";
+// import { createPresetSession, createCustomSession,  } from "./controllers/session/create";
 import { createPresetSession as createPresetV2, createCustomSession as createCustomV2 } from "./controllers/session/createV2";
 import { publishSession, getSessionById, getAllSessions } from "./controllers/session/basic";
 import passport from "passport";
@@ -8,10 +8,10 @@ const router = Router();
 
 router.get('/:id', passport.authenticate('jwt',{session: false}), getSessionById) //Simple Get route
 router.get('/', passport.authenticate('jwt',{session: false}), getAllSessions); //Get all sessions
-router.post('/preset', passport.authenticate('jwt',{session: false}), createPresetSession);
-router.post('/presetv2', passport.authenticate('jwt',{session: false}), createPresetV2);
-router.post('/custom', passport.authenticate('jwt',{session: false}), createCustomSession);
-router.post('/customv2', passport.authenticate('jwt',{session: false}), createCustomV2);
+router.post('/preset', passport.authenticate('jwt',{session: false}), createPresetV2);
+// router.post('/presetv2', passport.authenticate('jwt',{session: false}), createPresetV2);
+router.post('/custom', passport.authenticate('jwt',{session: false}), createCustomV2);
+// router.post('/customv2', passport.authenticate('jwt',{session: false}), createCustomV2);
 router.put('/:id/publish', passport.authenticate('jwt',{session: false}), publishSession);
 
 export default router;
