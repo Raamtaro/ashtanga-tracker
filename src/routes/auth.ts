8d23349af97e1b9202c0d24c09bb25b502a36357
Raam <raam.sanghani@gmail.com>
2026-01-07 16:31:29 -0600

    Updated the deleteAccount route to utilise post instead of delete and also added in passport.authenticate

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,12 @@
 import Router from "express";
 import { signup, loginUser, getAllUsers, deleteAccount } from "./controllers/auth";
+import passport from "passport";
 
 const router = Router();
 
 router.post('/signup', signup);
 router.post('/login', loginUser);
-router.delete('/delete', deleteAccount);
+router.post('/delete', passport.authenticate('jwt', { session: false }), deleteAccount);
 
 router.get('/all', getAllUsers)
 
