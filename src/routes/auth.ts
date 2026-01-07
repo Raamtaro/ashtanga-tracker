9312d989387005ac0dbac0e13530ccb21da21efc
Raam <raam.sanghani@gmail.com>
2026-01-07 16:29:31 -0600

    Added a delete route in the corresponding auth.ts route file

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,11 @@
 import Router from "express";
-import { signup, loginUser, getAllUsers } from "./controllers/auth";
+import { signup, loginUser, getAllUsers, deleteAccount } from "./controllers/auth";
 
 const router = Router();
 
 router.post('/signup', signup);
 router.post('/login', loginUser);
+router.delete('/delete', deleteAccount);
 
 router.get('/all', getAllUsers)
 
