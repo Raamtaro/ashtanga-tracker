6c20375a3a0483ee32aab8f1507877901d656001
Raam <raam.sanghani@gmail.com>
2026-02-10 11:50:16 -0600

    Added a health check route

---
+++ src/routes/controllers/health.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express'
+
+export const healthAndGutCheck = (req: Request, res: Response) => {
+    res.json(
+        {
+            ok: true,
+            env: process.env.NODE_ENV ?? 'unknown',
+            time: new Date().toISOString(),
+            commit: process.env.RAILWAY_GIT_COMMIT_SHA?.slice(0, 7) ?? null,
+        }
+    )
+};
---
+++ src/routes/health.ts
@@ -0,0 +1,8 @@
+import {Router} from 'express'
+import { healthAndGutCheck } from './controllers/health'
+
+const router = Router();
+
+router.get('/', healthAndGutCheck) // Mainly for beta testing - it is intended to show up in the client build on the login page so that I can verify how the environment variables are being set
+
+export default router
